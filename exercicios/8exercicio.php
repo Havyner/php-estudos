12a379e3819c7281a8bf87697325c16f31b345a5
Havyner Jalles Siqueira <havy012_@hotmail.com>
2020-06-11 19:13:37 -0300

    exercicio 8

--- exercicios/8exercicio.php
+++ exercicios/8exercicio.php
@@ -1,2 +1,12 @@
 8 - Faça um Programa que peça um número correspondente a um determinado ano 
-e em seguida informe se este ano é ou não bissexto. */
+e em seguida informe se este ano é ou não bissexto.</br></br>
+
+<?php
+
+$ano = 2020;
+
+if($ano%400 == 0 || ($ano%4 == 0 && $ano%100 != 0)){
+    echo "$ano é Bisexto!";
+}else {
+    echo "$ano Não é Bisexto!";
+}
