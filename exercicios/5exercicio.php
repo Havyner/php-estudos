a3edab82222e9ff33c613ec5f9c5657750a9bc09
Havyner Jalles Siqueira <havy012_@hotmail.com>
2020-06-11 16:48:19 -0300

    exercicio 5

--- exercicios/5exercicio.php
+++ exercicios/5exercicio.php
@@ -1,1 +1,16 @@
-//5 - Faça um Programa que peça dois números e imprima o maior deles. Exibir os valores informados.
+5 - Faça um Programa que peça dois números e imprima o maior deles. Exibir os valores informados.
+
+<?php
+
+$numero1 = 55;
+$numero2 = 24;
+
+echo "</br></br>O maior número é: ";
+
+if($numero1 == $numero2){
+    echo "Números iguais!!";
+}else if($numero1 > $numero2){
+    echo "$numero1";
+}else{
+    echo "$numero2";
+}
