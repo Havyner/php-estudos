6f1bdf654ffc7895d3106b88c4c256c23d970e07
Havyner Jalles Siqueira <havy012_@hotmail.com>
2020-06-11 15:23:45 -0300

    exercicio 3

--- exercicios/3exercicio.php
+++ exercicios/3exercicio.php
@@ -1,1 +1,9 @@
-/*3 - Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58. */
+3 - Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo 
+que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58.
+
+<?php
+
+$altura = 1.72;
+$peso_ideal = (72.7 * $altura) - 58;
+
+echo "</br></br>De acordo com sua altura $altura </br>Seu peso ideal é $peso_ideal";
